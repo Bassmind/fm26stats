ab20b6b821d8b1c7c287de4f99ea01d023d929a9
David Mendiola <isc.david.mendiola@hotmail.com>
2026-01-27 02:27:51 -0600

    Especificacion de data a extraer, y hice merge de dos metodos en uno

--- src/modulos/carga_archivos.py
+++ src/modulos/carga_archivos.py
@@ -6,11 +6,49 @@ Responsabilidades:
 - Reconocer las columnas y eliminar las primeras filas que no sirven
 - Regresar un arreglo/mapa con cada fila y su data
 - Permitir activar/desactivar bandera para shortlist o búsqueda de jugadores
+
+ESTRUCTURA DEL ARCHIVO FM GENIE SCOUT INICIAL, (REQUIRED) = Data a extraer:
+* Gen
+* Name (REQUIRED) -> Nombre del jugador
+* Nation
+* Club (REQUIRED) -> Club actual
+* Age (REQUIRED) -> Edad (Aunque puede ser incorrecta)
+* Value -> Valor del jugador
+* Sale Value -> Valor de venta del jugador
+* Best Pot Rating -> Mejor posicion
+* GK - Ball-Playing Goalkeeper - IP (REQUIRED)
+* GK - Ball-Playing Goalkeeper - IP (Pot) (REQUIRED)
+* GK - Sweeper Keeper - OOP (REQUIRED)
+* GK - Sweeper Keeper - OOP (Pot) (REQUIRED)
+* WB - Advanced Wing-Back - IP (REQUIRED)
+* WB - Advanced Wing-Back - IP (Pot) (REQUIRED)
+* D (RL) - Full-Back - OOP (REQUIRED)
+* D (RL) - Full-Back - OOP (Pot) (REQUIRED)
+* D (C) - Ball-Playing Centre-Back - IP (REQUIRED)
+* D (C) - Ball-Playing Centre-Back - IP (Pot) (REQUIRED)
+* D (C) - Overlapping Centre-Back - IP (REQUIRED)
+* D (C) - Overlapping Centre-Back - IP (Pot) (REQUIRED)
+* D (C) - Centre-Back - OOP (REQUIRED)
+* D (C) - Centre-Back - OOP (Pot) (REQUIRED)
+* M (C) - Attacking Midfielder - IP (REQUIRED)
+* M (C) - Attacking Midfielder - IP (Pot) (REQUIRED)
+* DM - Defensive Midfielder - OOP (REQUIRED)
+* DM - Defensive Midfielder - OOP (Pot) (REQUIRED)
+* AM (RL) - Inside Winger - IP (REQUIRED)
+* AM (RL) - Inside Winger - IP (Pot) (REQUIRED)
+* AM (RL) - Inside Forward - IP (REQUIRED)
+* AM (RL) - Inside Forward - IP (Pot) (REQUIRED)
+* AM (RL) - Winger - OOP (REQUIRED)
+* AM (RL) - Winger - OOP (Pot) (REQUIRED)
+* AM (C) - Second Striker - IP (REQUIRED)
+* AM (C) - Second Striker - IP (Pot) (REQUIRED)
+* AM (C) - Attacking Midfielder - OOP (REQUIRED)
+* AM (C) - Attacking Midfielder - OOP (Pot) (REQUIRED)
 """
 
-def cargar_archivo(ruta_archivo=None):
+def cargar_extraccion_archivo(ruta_archivo=None):
     """
-    Carga un archivo exportado desde FM Genie Scout.
+    Carga un archivo exportado desde FM Genie Scout y se extrae la data
     
     Args:
         ruta_archivo (str, optional): Ruta del archivo a cargar.
